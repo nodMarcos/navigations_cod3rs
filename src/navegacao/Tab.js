96fde4d940dafbed155a204f589a6732ad30953d
Marcos <nodmarcos@gmail.com>
2022-05-29 21:36:19 -0300

    added one more screen and icons to the tab navigation

--- src/navegacao/Tab.js
+++ src/navegacao/Tab.js
@@ -1,4 +1,5 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons'
 import TelaA from '../views/TelaA';
 import TelaB from '../views/TelaB';
 import TelaC from '../views/TelaC';
@@ -7,23 +8,42 @@ const Tab = createBottomTabNavigator();
 
 export default props => {
   return (
-   <Tab.Navigator screenOptions={{
-    tabBarActiveTintColor: "red",
-    tabBarInactiveTintColor: "blue",
-    tabBarLabelStyle: {
-      fontSize: 30
-    },
-    tabBarStyle: [
-      {
-        display: "flex"
+    <Tab.Navigator screenOptions={({ route }) => ({
+      tabBarIcon: ({ focused, color, size }) => {
+
+        let iconName;
+        if (route.name === 'TelaA') {
+          iconName = focused ?
+           'information-circle-outline' : 
+           'information-outline' ;
+        } else if (route.name === 'TelaB') {
+          iconName = focused ?
+          'information-circle-outline' : 
+          'information-outline' ;
+        } else if (route.name === 'TelaC') {
+          iconName = focused ?
+          'list-outline' : 
+          'list' ;
+        }
+        return <Ionicons name={iconName} size={size} color={color} />;
       },
-      null,
-    ]
-   }} initialRouteName="TelaB">
-     <Tab.Screen name="TelaA" component={TelaA} />
+      tabBarActiveTintColor: "red",
+      tabBarInactiveTintColor: "blue",
+      // tabBarShowLabel: false
+      // tabBarLabelStyle: {
+      //   showLabel: false
+      // },
+      tabBarStyle: [
+        {
+          display: "flex"
+        },
+        null,
+      ]
+    })} initialRouteName="TelaB">
+      <Tab.Screen name="TelaA" component={TelaA} />
       <Tab.Screen name="TelaB" component={TelaB} />
       <Tab.Screen name="TelaC" component={TelaC} />
 
-   </Tab.Navigator>
+    </Tab.Navigator>
   );
 }
--- src/navegacao/index.js
+++ src/navegacao/index.js
@@ -2,15 +2,15 @@ import React from 'react'
 import { SafeAreaView } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 
-import Drawer from './Drawer'
-// import Tab from './Tab'
+// import Drawer from './Drawer'
+import Tab from './Tab'
 // import Stack from './Stack'
 
 export default props => (
     <SafeAreaView style={{flex: 1}}>
         <NavigationContainer>
-            <Drawer />
-            {/* <Tab /> */}
+            {/* <Drawer /> */}
+            <Tab />
             {/* <Stack /> */}
         </NavigationContainer>
     </SafeAreaView>
